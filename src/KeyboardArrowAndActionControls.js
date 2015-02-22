95a63fcf810e31a2af3b18ff5347629a9d32cca9
Mr Speaker <mrspeaker@gmail.com>
2015-02-22 09:51:19 -0500

    class up the keys

--- src/KeyboardArrowAndActionControls.js
+++ src/KeyboardArrowAndActionControls.js
@@ -1,46 +1,43 @@
-function KeyboardArrowAndActionControls () {
+class KeyboardArrowAndActionControls {
 
-  this._keys = {};
+  constructor () {
+    this._keys = {};
+    document.body.addEventListener("keydown", this._onDown.bind(this), false);
+    document.body.addEventListener("keyup", this._onUp.bind(this), false);
+  }
 
-  document.body.addEventListener("keydown", this._onDown.bind(this), false);
-  document.body.addEventListener("keyup", this._onUp.bind(this), false);
-}
-
-KeyboardArrowAndActionControls.prototype = {
-
-  _onDown: function (e) {
+  _onDown (e) {
     if ([37,38,39,40].indexOf(e.which) >= 0) {
       e.preventDefault();
     }
     this._keys[e.which] = 1;
-  },
+  }
 
-  _onUp: function (e) {
+  _onUp (e) {
     this._keys[e.which] = 0;
-  },
+  }
 
-  action: function () {
+  action () {
     return this._keys[32];
-  },
+  }
 
-  rot: function () {
+  rot () {
     var left = (this._keys[37]) ? 1 : 0;
     var right = (this._keys[39]) ? 1 : 0;
     return -left +right;
-  },
+  }
 
-  x: function () {
+  x () {
     var left = (this._keys[81] || this._keys[65]) ? 1 : 0;
     var right = (this._keys[68]) ? 1 : 0;
     return -left +right;
-  },
+  }
 
-  y: function () {
+  y () {
     var up = (this._keys[90] || this._keys[87]) ? 1 : 0;
     var down = (this._keys[83]) ? 1 : 0;
     return -up +down;
   }
-
-};
+}
 
 export default KeyboardArrowAndActionControls;
