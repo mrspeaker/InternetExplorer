7a5c22eb53c5019eb84501a26a7db401fedc1f30
Mr Speaker <mrspeaker@gmail.com>
2015-03-02 13:29:09 -0500

    more clean

--- src/world/ground.js
+++ src/world/ground.js
@@ -4,14 +4,11 @@ grassTex.wrapS = THREE.RepeatWrapping;
 grassTex.wrapT = THREE.RepeatWrapping;
 grassTex.repeat.x = 2046;
 grassTex.repeat.y = 2046;
-const groundMat = new THREE.MeshBasicMaterial({map:grassTex});
 
-const geometry = new THREE.PlaneBufferGeometry( 10000, 10000 );
-/*const material = new THREE.MeshLambertMaterial({
-  color: 0x26463E
-});*/
+const ground = new THREE.Mesh(
+  new THREE.PlaneBufferGeometry( 10000, 10000 ),
+  new THREE.MeshBasicMaterial({ map: grassTex }) );
 
-const ground = new THREE.Mesh( geometry, groundMat );
 ground.position.y = 0;
 ground.rotation.x = - Math.PI / 2;
 ground.renderDepth = 2;
